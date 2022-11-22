c2842aaef341c8d3ee42bd00b330e479bdb46eae
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-22 16:37:17 -0500

    Add route for template into app

--- app.py
+++ app.py
@@ -30,3 +30,10 @@ def handler(req, resp):
     resp.text = "sample"
 
 app.add_route("/sample", handler)
+
+@app.route("/template")
+def template_handler(req,resp):
+    resp.body = app.template(
+        "index.html", context = {
+            "name" : "Python Full Stack Practice",
+            "title" : "My Python Framework"}).encode()
