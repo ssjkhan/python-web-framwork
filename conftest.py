61af42ff39a7f22347a4eb32beeab9f2ea6c0e03
SK <sk@debian-BULLSEYE-live-builder-AMD64>
2022-11-23 17:31:18 -0500

    Implement Database and Table Creation for sqlite database service

--- conftest.py
+++ conftest.py
@@ -8,3 +8,31 @@ def api():
 @pytest.fixture
 def client(api):
     return api.test_session()
+
+
+@pytest.fixture
+def db():
+    DB_PATH = "./test.db"
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+    db = Database(DB_PATH)
+    return db
+
+
+@pytest.fixture
+def Author():
+    class Author(Table):
+        name = Column(str)
+        age = Column(int)
+
+    
+    return Author
+
+@pytest.fixture
+def Book(Author):
+    class Book(Table):
+        title = Column(str)
+        published = Column(bool)
+        author = ForeignKey(Author)
+
+    return Book
